540ee442cbd33327227770123f299ab4b86deb35
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-11-08 18:08:49 +0900

    Update Drawing-Book.cpp

--- Drawing-Book.cpp
+++ Drawing-Book.cpp
@@ -1,3 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
 int pageCount(int n, int p)
 {
     return std::min(p/2, n/2 - p/2);
